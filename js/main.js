7500b5c60add6b28a33cd973dece143b074a7ba7
Eno <urboieno12@gmail.com>
2022-07-23 18:55:29 +0900

    nav buttons finished

--- js/main.js
+++ js/main.js
@@ -1,55 +1,41 @@
-const menuBtn = document.querySelector('.fa-circle-chevron-down');
-const menuBtnX = document.querySelector('.fa-arrow-right-from-bracket')
+
+
 const navBar = document.querySelector('.navbar');
 
+const menuBtn = document.querySelector('.fa-circle-chevron-down');
+const menuBtnX = document.querySelector('.fa-arrow-right-from-bracket')
 
-let menuOpen = false;
 
+const menuItem1 = document.querySelector('.items')
+const menuItem2 = document.querySelector('.item2')
+const menuItem3 = document.querySelector('.item3')
+const menuItem4 = document.querySelector('.item4')
+
+const newArr = [menuBtnX, menuItem1, menuItem2, menuItem3, menuItem4]
 
 menuBtn.addEventListener('click', () => {
-
-    if(!menuOpen){
+    let menuOpen = false;
+    if(!menuOpen) {
         menuBtn.classList.add('hidden');
-        navBar.classList.remove('hidden');
-        navBar.classList.add('show');
 
-
-        menuBtnX.classList.add('show');
+        navBar.classList.remove('hidden')
         menuBtnX.classList.remove('hidden');
-        menuOpen = true;
-    
-        if(menuOpen){
-        menuBtnX.addEventListener('click', () => {
-        if (menuOpen) {
-    
-
-        menuBtnX.classList.remove('show');
-        menuBtnX.classList.add ('hidden');
-
-        navBar.classList.add('hidden');
-        menuBtn.classList.add('show');
 
         menuOpen = true;
-        }})
+        if (menuOpen){
 
+            newArr.map(ele => {
+                ele.addEventListener('click', () => {
+                menuBtn.classList.remove('hidden');
+
+                navBar.classList.add('hidden')
+                menuBtnX.classList.add('hidden');
+                menuOpen = true;
+                })
+            })
         }
     }
-
 })
 
 
 
-menuBtnX.addEventListener('click', () => {
-        if (menuOpen) {
-    
-
-        menuBtnX.classList.remove('show');
-        menuBtnX.classList.add ('hidden');
-
-        navBar.classList.add('hidden');
-        menuBtn.classList.add('show');
-
-        menuOpen = true;
-        }}
-        )
-    
